34b31c89713e7c0c9207934c1ade581fffb20515
Code-Kat <kd.re@live.ca>
2021-05-28 12:21:50 +0900

    step 2: fetched data from Unsplash API and logged data to console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import react, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="app"></div>;
+  //const [images, setImages] = useState([]);
+  const apiKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
+  useEffect(() => {
+    fetch("https://api.unsplash.com/search/collections?page=1&query=spacex", {
+      headers: {
+        Authorization: `Client-ID ${apiKey}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [apiKey]);
+  return <div className="app">hi</div>;
 }
 
 export default App;
